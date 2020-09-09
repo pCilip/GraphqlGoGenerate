97782c3db9b0ff4e8c3dee27dd3f8bfba462a49b
Peter Cilip <pwcilip@gmail.com>
2020-09-09 13:27:29 +0200

    fix generating imports in package

--- internal/Generator/TypeBuilder.go
+++ internal/Generator/TypeBuilder.go
@@ -14,15 +14,15 @@ const (
 	OK
 )
 
-func BuildParamType(generator *Generator, inputValue Schema.InputValue, generatedField *jen.Statement) EndType {
-	return BuildTypeFromReference(generator, &inputValue.Type, generatedField, true)
+func BuildParamType(generator *Generator, parentPackage string, inputValue Schema.InputValue, generatedField *jen.Statement) EndType {
+	return BuildTypeFromReference(generator, parentPackage, &inputValue.Type, generatedField, true)
 }
 
-func BuildFieldType(generator *Generator, field Schema.Field, generatedField *jen.Statement) EndType {
-	return BuildTypeFromReference(generator, &field.Type, generatedField, true)
+func BuildFieldType(generator *Generator, parentPackage string, field Schema.Field, generatedField *jen.Statement) EndType {
+	return BuildTypeFromReference(generator, parentPackage, &field.Type, generatedField, true)
 }
 
-func BuildTypeFromReference(generator *Generator, typeRef Schema.TypeReference, generatedField *jen.Statement, addNull bool) EndType {
+func BuildTypeFromReference(generator *Generator, parentPackage string, typeRef Schema.TypeReference, generatedField *jen.Statement, addNull bool) EndType {
 
 	if typeRef == nil {
 		return OK
@@ -35,27 +35,27 @@ func BuildTypeFromReference(generator *Generator, typeRef Schema.TypeReference, 
 			generatedField.Id("*")
 		}
 		generatedField.Id("[]")
-		return BuildTypeFromReference(generator, typeRef.SubType(), generatedField, true)
+		return BuildTypeFromReference(generator, parentPackage, typeRef.SubType(), generatedField, true)
 	// nullable type
 	case Schema.OBJECT:
-		return GenerateName(generator, generatedField, typeRef.GetName())
+		return GenerateName(generator, parentPackage, generatedField, typeRef.GetName())
 
 	case Schema.SCALAR, Schema.ENUM, Schema.INPUT_OBJECT:
 		if addNull {
 			generatedField.Id("*")
 		}
 
-		return GenerateName(generator, generatedField, typeRef.GetName())
+		return GenerateName(generator, parentPackage, generatedField, typeRef.GetName())
 	// non nullable type
 	case Schema.NON_NULL:
-		return BuildTypeFromReference(generator, typeRef.SubType(), generatedField, false)
+		return BuildTypeFromReference(generator, parentPackage, typeRef.SubType(), generatedField, false)
 
 	default:
 		return CONTINUE
 	}
 }
 
-func GenerateName(generator *Generator, generatedField *jen.Statement, name *string) EndType {
+func GenerateName(generator *Generator, parentPackage string, generatedField *jen.Statement, name *string) EndType {
 
 	if name == nil {
 		return RETURN
@@ -65,7 +65,14 @@ func GenerateName(generator *Generator, generatedField *jen.Statement, name *str
 	if ok {
 		importPath, found := generator.findImportPath(typeVal)
 		if found {
-			generatedField.Qual(importPath, typeVal)
+			// importing model to same model (parent-child relations)
+			// import without global path
+			if importPath == parentPackage {
+				generatedField.Qual("", typeVal)
+			} else {
+				generatedField.Qual(importPath, typeVal)
+			}
+
 		} else {
 			return RETURN
 		}
